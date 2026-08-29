c64fdd742343ce5e52f830eb9ac88db7b76d2b6f
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2026-08-29 17:01:02 +0500

    refactor!: remove predictions tensor from return tuple of `quantify_uncertainty` function
    The reason is that the function clearly states that it is "quantifying uncertainties", thus there is no need to return predicted class from `mc_preds` input tensor. User can do it himself by using `argmax` function.

--- utils/uncertainty.py
+++ utils/uncertainty.py
@@ -8,7 +8,11 @@ from torch import Tensor
 def mc_predict(model: nn.Module, x: Tensor, mc_samples: int = 10) -> Tensor:
     """
     Runs T stochastic forward passes.
-    Returns tensor shape: [T, batch, num_classes]
+
+    :param model: The model to evaluate.
+    :param x: Input tensor of shape [batch, ...].
+    :param mc_samples: Number of Monte Carlo samples (forward passes).
+    :return: Tensor shape: [T, batch, num_classes]
     """
     model.train()
     preds = []
@@ -22,7 +26,18 @@ def mc_predict(model: nn.Module, x: Tensor, mc_samples: int = 10) -> Tensor:
 
 
 @torch.no_grad()
-def quantify_uncertainties(mc_preds: Tensor) -> tuple[Tensor, tuple[Tensor, Tensor, Tensor]]:
+def quantify_uncertainties(mc_preds: Tensor) -> tuple[Tensor, Tensor, Tensor]:
+    """
+    Quantifies aleatoric and epistemic uncertainties from MC predictions.
+    :param mc_preds: Class probabilities from `T` Monte Carlo forward passes.
+            Shape: `[T, B, C]`, where:
+                `T` = number of MC samples,
+                `B` = batch size,
+                `C` = number of classes.
+            Each mc_preds[t, b] is a probability vector over C classes.
+    :return: Tuple of (total, aleatoric, epistemic)
+            uncertainty matrices, each of shape [B, C, C].
+    """
     T = mc_preds.shape[0]
     # Средние вероятности по T проходам: [batch_size, num_classes]
     mean_probs = torch.mean(mc_preds, dim=0)
@@ -36,4 +51,4 @@ def quantify_uncertainties(mc_preds: Tensor) -> tuple[Tensor, tuple[Tensor, Tens
     deviation = mc_preds - mean_probs
     epistemic = torch.einsum('tbi,tbj->bij', deviation, deviation) / T
 
-    return mean_probs.argmax(dim=1), (aleatoric + epistemic, aleatoric, epistemic)
+    return aleatoric + epistemic, aleatoric, epistemic
